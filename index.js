2bd04fc201a37ce1fd6e1b2b56580a645b8fd51c
Ammar Zulfiqar <ammarzulfiqar@Ammars-MacBook-Air.local>
2024-07-21 23:50:50 +0500

    slash problem

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://ahkwebsite.vercel.app/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://ahkwebsite.vercel.app"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", true); // allows cookie to be sent
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, HEAD, DELETE"); // you must specify the methods used with credentials. "*" will not work. 
